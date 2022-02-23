9cdd4b163d6750d7d69c70737a7f32146eaf2623
j2zhao <j2zhao@uchicago.edu>
2022-02-23 15:41:50 -0600

    new server writes

--- logistic_regression/server.py
+++ logistic_regression/server.py
@@ -22,22 +22,24 @@ def get_eval_fn(model: LogisticRegression):
         utils.set_model_params(model, parameters)
         loss = log_loss(y_test, model.predict_proba(X_test))
         accuracy = model.score(X_test, y_test)
+        end = time.time()
         with open('log_accuracy.txt', 'a') as f:
             f.write("loss: {}, accuracy: {} \n".format(loss, accuracy))
+        with open('log_performance.txt', 'a') as f:
+            f.write("time: {} \n".format(end))
         return loss, {"accuracy": accuracy}
 
     return evaluate
 
 class TimeStrategy(fl.server.strategy.FedAvg):
 
-    def configure_fit(self, rnd, weights, client_manager):
+    def configure_fit(self, *args):
         if start == 0:
             start = time.time()
-        return super.configure_fit(rnd,weights, client_manager)
+        return super.configure_fit(*args)
 
-
-    def evaluate(self, weights):
-        result = super().evaluate(weights)
+    def evaluate(self, *args):
+        result = super().evaluate(*args)
         with open('log_performance.txt', 'a') as f:
             f.write("time: {} \n".format(end - start))
         return result 
@@ -45,14 +47,16 @@ class TimeStrategy(fl.server.strategy.FedAvg):
 
 if __name__ == "__main__":
     start = 0
+    end = 0
     model = LogisticRegression()
     utils.set_initial_params(model)
-    strategy = TimeStrategy(
-        min_available_clients=8,
+    strategy = fl.server.strategy.FedAvg(
+        min_available_clients=2,
         eval_fn=get_eval_fn(model),
         on_fit_config_fn=fit_round,
         min_fit_clients = 2
     )
+    #10.128.0.2:5040
     fl.server.start_server("10.128.0.2:5040", strategy=strategy, config={"num_rounds": 200})
     end = time.time()
     print(end-start)
